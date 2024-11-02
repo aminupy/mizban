5b12881e0f6d548048a52dacc63936bee1f6382a
Amin <esaminu.py@gmail.com>
2024-11-02 17:59:01 +0330

    -- prepared for executable releases

--- app/core/config.py
+++ app/core/config.py
@@ -4,21 +4,14 @@ import logging
 
 
 class Settings:
-    _BASE_DIR = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
 
     @property
     def UPLOAD_DIR(self):
-        upload_dir = os.path.abspath(os.path.join(self._BASE_DIR, "../../MizbanShared"))
+        if getattr(sys, 'frozen', False):  # Running as a PyInstaller bundle
+            base_dir = os.path.dirname(sys.executable)
+        else:  # Running as a regular script
+            base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 
-        try:
-            # Ensure the directory exists
-            if not os.path.exists(upload_dir):
-                os.makedirs(upload_dir)
-        except OSError as e:
-            logging.error(f"Failed to create directory {upload_dir}: {e}")
-            raise e
-
-        return upload_dir
-
+        return os.path.join(base_dir, "MizbanShared")
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+import uvicorn
+import orjson
+
 from app.api.endpoints import files
 from app.core import settings
+from app.utils.folder_util import initialize_shared_folder
 
+initialize_shared_folder(settings.UPLOAD_DIR)
 app = FastAPI()
 
 # Include routers for different parts of the API
@@ -10,3 +15,8 @@ app.include_router(files.router, prefix="/api/files", tags=["Files"])
 
 # Mount the shared files directory as a static path
 app.mount("/storage", StaticFiles(directory=settings.UPLOAD_DIR), name="MizbanShared")
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000, loop='uvloop', http='httptools')
